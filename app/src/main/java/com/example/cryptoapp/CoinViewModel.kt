b1f0bf730e336488ba848d56da80f410f7939302
BIBLETUM <pasha.remizov@mail.ru>
2024-02-05 15:58:07 +0300

    Added ability to get data from API and write it to DB

---
+++ app/src/main/java/com/example/cryptoapp/CoinViewModel.kt
@@ -0,0 +1,58 @@
+package com.example.cryptoapp
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import com.example.cryptoapp.api.ApiFactory
+import com.example.cryptoapp.database.AppDataBase
+import com.example.cryptoapp.pojo.CoinPriceInfo
+import com.example.cryptoapp.pojo.CoinPriceInfoRawData
+import com.google.gson.Gson
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class CoinViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val db = AppDataBase.getInstance(application)
+    private val compositeDisposable = CompositeDisposable()
+    val priceLis = db.coinPriceInfoDao().getPriceList()
+
+    fun loadData() {
+        val disposable = ApiFactory.apiService.getTopCoinsInfo()
+            .map { it.data?.map { it.coinInfo?.name }?.joinToString(",").toString() }
+            .flatMap { ApiFactory.apiService.getFullPriceList(fSyms = it) }
+            .map { getPriceListFromRawData(it) }
+            .subscribeOn(Schedulers.io())
+            .subscribe({
+                db.coinPriceInfoDao().insertPriceList(it)
+                Log.d("aboba", it.toString())
+            }, {
+                Log.d("Aboba", it.message.toString())
+            })
+        compositeDisposable.add(disposable)
+    }
+
+    private fun getPriceListFromRawData(coinPriceInfoRawData: CoinPriceInfoRawData): List<CoinPriceInfo> {
+        val result = ArrayList<CoinPriceInfo>()
+        val jsonObject = coinPriceInfoRawData.coinPriceInfoJsonObject
+        if (jsonObject == null) return result
+        val coinKeySet = jsonObject.keySet()
+        for (key in coinKeySet) {
+            val currencyJson = jsonObject.getAsJsonObject(key)
+            val currencyKeySet = currencyJson.keySet()
+            for (currencyKey in currencyKeySet) {
+                val priceInfo = Gson().fromJson(
+                    currencyJson.getAsJsonObject(currencyKey),
+                    CoinPriceInfo::class.java
+                )
+                result.add(priceInfo)
+            }
+        }
+        return result
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+}
--- app/src/main/java/com/example/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/example/cryptoapp/MainActivity.kt
@@ -3,10 +3,27 @@ package com.example.cryptoapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.cryptoapp.api.ApiFactory
+import com.example.cryptoapp.api.ApiService
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import org.jetbrains.annotations.Async.Schedule
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: CoinViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
+        viewModel.loadData()
+        viewModel.priceLis.observe(this, Observer {
+            Log.d("ABOBA", it.toString())
+        })
     }
 }
